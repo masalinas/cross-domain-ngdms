c1051f2d5401d9d721272ccb6973e4ee70f23dc6
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2023-02-15 12:17:25 +0100

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cross-domain-proxy-ngdms';
+export class AppComponent implements OnInit {
+  title = 'Cross Domain Proxy NGDMS';
+  sampleToken: string;
+
+  constructor(private cookieService: CookieService) {    
+  }
+
+  ngOnInit() {
+    this.sampleToken = this.cookieService.get('token');
+  }
 }
